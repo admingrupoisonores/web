e39565a49bfaac4d2b80ae364c3a2ce9694d0265
Alexandre D. Díaz <dev@redneboa.es>
2024-03-22 12:14:07 +0100

    add: web_field_reference_domain

---
+++ web_field_reference_domain/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2024 Alexandre D. Díaz - Grupo Isonor
+
+{
+    "name": "Web Field Reference Domain",
+    "summary": "Add support to define domains on reference fields",
+    "version": "14.0.1.0.0",
+    "category": "Web",
+    "website": "",
+    "author": "Grupo Isonor",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["web"],
+    "data": [],
+    "demo": [],
+    "assets": {
+        "web.assets_backend": [
+            "web_field_reference_domain/static/src/js/basic_model.js",
+            "web_field_reference_domain/static/src/js/relational_fields.js",
+        ],
+    },
+}
